989b9c67b8202eb9d4d22ad97702de6673100dd8
Edwin Chi <chipineiro@gmail.com>
2023-03-07 11:51:52 -0600

    Integrate TestProperties to BaseTest

--- src/test/java/components/BaseTest.java
+++ src/test/java/components/BaseTest.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.LoginPage;
+import resources.utils.TestProperties;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -14,14 +15,13 @@ import java.util.Properties;
 public class BaseTest {
 
     private WebDriver driver;
-    private Properties prop;
+    private TestProperties prop;
 
 
     protected WebDriver initializeDriver() throws IOException {
-        prop = loadProperties();
-        String browserName = prop.getProperty("browser");
+        prop = new TestProperties();
 
-       if(browserName.equalsIgnoreCase("chrome")){
+       if(prop.getProperties().getProperty("browser").equalsIgnoreCase("chrome")){
            WebDriverManager.chromedriver().setup();
            driver = new ChromeDriver();
            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -31,17 +31,10 @@ public class BaseTest {
        return driver;
     }
 
-    private Properties loadProperties() throws IOException {
-        Properties prop = new Properties();
-        FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "//src/main//java//resources//properties//global-data.properties");
-        prop.load(fis);
-        return prop;
-    }
-
     protected LoginPage launchApplication() throws IOException {
         driver = initializeDriver();
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.goTo(prop.getProperty("url"));
+        loginPage.goTo(prop.getProperties().getProperty("url"));
         return loginPage;
     }
 
