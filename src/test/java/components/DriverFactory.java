ebe05f4d1cc66485faf404cc65c0be1d876da0ed
Edwin Chi <chipineiro@gmail.com>
2023-04-03 17:46:03 -0600

    Create DriverFactory

---
+++ src/test/java/components/DriverFactory.java
@@ -0,0 +1,34 @@
+package components;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import resources.utils.TestProperties;
+
+import java.time.Duration;
+import java.util.Properties;
+
+public class DriverFactory {
+    private WebDriver driver;
+    private Properties prop;
+
+    public WebDriver getDriver(){
+        prop = new Properties();
+        prop = new TestProperties().getProperties();
+
+        if(prop.getProperty("browser").equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
+
+    public void closeDriver(){
+        if(driver != null){
+            driver.quit();
+            driver = null;
+        }
+    }
+}
